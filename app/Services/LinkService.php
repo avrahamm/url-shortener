a4caeb132e62557e7db52c7691c9ed201479bab3
avrahamm <papaabramg@gmail.com>
2026-01-13 19:29:39 +0200

    Split slug generation for easier testing

--- app/Services/LinkService.php
+++ app/Services/LinkService.php
@@ -7,10 +7,20 @@ use Faker\Provider\Uuid;
 
 class LinkService
 {
+    /**
+     * Returns six chars long slug.
+     *
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return Uuid::bothify('******');
+    }
+
     public function getUniqueSlug(): string
     {
         while (true) {
-            $slug = Uuid::bothify('******');
+            $slug = $this->getSlug();
             if (Link::where('slug', $slug)->exists()) {
                 continue;
             }
