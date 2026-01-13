a16c842f3d10520faeaedf3b48e6578d8bfa0516
avrahamm <papaabramg@gmail.com>
2026-01-13 22:17:27 +0200

    DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Link;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         Link::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $targetUrl = 'https://apple.com';
+        $slug = 'apple';
+
+        Link::factory()->create([
+            'target_url' => $targetUrl,
+            'slug' => $slug
+        ]);
     }
 }
